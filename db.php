16ce566bbd142f500282523a05a4a909d1f55b47
oakbaza <oakbaza@gmail.com>
2018-10-29 07:15:24 +0700

    Error select

--- db.php
+++ db.php
@@ -1,14 +1,22 @@
 <?php
-$servername = "172.30.139.241";
-$username = "root";
+$servername = getenv("MYSQL_SERVICE_HOST");
+$username ="root";
 $password = "root";
-
+$dbname = "project";
+echo $servername."<br>".$username."<br>".$password."<br>";
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password,$dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully";
+
+function get_all(){
+	$sql = "SELECT * 
+			FROM person";
+	$query = $conn->query($sql);
+	return $query;
+}
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 include_once('db.php');
+$test = get_all();
 	echo "Edit from local";
 	echo "<br>Commit and push";
 	echo "<br>Webhook demo";
