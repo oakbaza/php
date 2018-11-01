ea688701e73f8b27c2f0811c3abeb6a1c157857f
oakbaza <oakbaza@gmail.com>
2018-11-01 14:19:34 +0700

    Web hook

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php 
 include_once('db.php');
 print_r($query->fetch_assoc());
-	echo "Edit from local";
-	echo "<br>Commit and push";
-	echo "<br>Webhook demo";
-	echo "<br>pod";
 ?>
 <h1>Hello World!</h1>
+
+<table>
+	<thead>
+		<tr>
+			<th>Firstname</th>
+			<th>Lastname</th>
+		</tr>
+	</thead>
+<?php	foreach($query->fetch_assoc() as $row){?>
+	<tr>
+		<td><?php echo $row->firstname?></td>
+		<td><?php echo $row->lnamename?></td>
+	</tr>
+<?php	} ?>
+</table>
