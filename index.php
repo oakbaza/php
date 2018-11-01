49a1fa94cdaafb8c9b213342c1ab61dec1512c92
oakbaza <oakbaza@gmail.com>
2018-11-01 18:06:30 +0700

    Add decribe text

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ include_once('db.php');
 $t7_db = connect();
 $obj_ps = get_all($t7_db);
 ?>
-<h1>Hello World!</h1>
-
-<<table border="1px black">
+<h1>Hello Myphp From Team7!</h1>
+<h1>Add Mysql</h1>
+<table border="1px black">
 	<thead>
 		<tr>
 			<th>Firstname</th>
