e3aa47edae98239c7ca925cdf739d3ef75c0b8be
oakbaza <oakbaza@gmail.com>
2018-11-01 14:13:12 +0700

    Web hook

--- db.php
+++ db.php
@@ -10,11 +10,12 @@ $conn = new mysqli($servername, $username, $password,$dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+} else{
+	echo "Connected successfully";
+}
 
 $sql = "SELECT * 
 		FROM person";
 $query = $conn->query($sql);
-print_r($query->fetch_assoc());
+
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php 
 include_once('db.php');
+print_r($query->fetch_assoc());
 	echo "Edit from local";
 	echo "<br>Commit and push";
 	echo "<br>Webhook demo";
 	echo "<br>pod";
 ?>
+<h1>Hello World!</h1>
