40644efc5ebdac7555b96ff81a64381a3a269c99
oakbaza <oakbaza@gmail.com>
2018-11-02 08:18:53 +0700

    Add bootstrap

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<style>
-table {
-    border-collapse: collapse;
-	width: 60%;
-}
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-table, th, td {
-    border: 1px solid black;
-}
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style>
 th,td{
 	text-align:center;
 }
@@ -18,17 +18,21 @@ $obj_ps = get_all($t7_db);
 ?>
 <h1>Hello Myphp From Team7!</h1>
 <h1>Add Mysql</h1>
-<table border="1px black">
-	<thead>
+<div class="container">
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th class="col-md-">Firstname</th>
+				<th>Lastname</th>
+			</tr>
+		</thead>
+		<tbody>
+	<?php	while($obj = mysqli_fetch_object($obj_ps)){?>
 		<tr>
-			<th>Firstname</th>
-			<th>Lastname</th>
+			<td><?php echo $obj->firstname;?></td>
+			<td><?php echo $obj->lastname;?></td>
 		</tr>
-	</thead>
-<?php	while($obj = mysqli_fetch_object($obj_ps)){?>
-	<tr>
-		<td><?php echo $obj->firstname;?></td>
-		<td><?php echo $obj->lastname;?></td>
-	</tr>
-<?php	} ?>
-</table> 
+	<?php	} ?>
+		</tbody>
+	</table> 
+</div>
